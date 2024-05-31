073e54dab0e4a5488fd6ba389f36bf61a676a229
Gianmarco <79141407+Gamma5oo@users.noreply.github.com>
2024-05-31 11:04:45 +0200

    Update script-ch10.lua

--- game/scripts/ita/script-ch10.lua
+++ game/scripts/ita/script-ch10.lua
@@ -6,30 +6,30 @@ function ch10script()
 	if cl > 150 then
 		scriptJump(1)
 	elseif cl == 1 then
-	bgUpdate('residential')
-	audioUpdate('2g')
-	cw(s_name,gtext)
+		bgUpdate('residential')
+		audioUpdate('2g')
+		cw(s_name,gtext)
 	elseif cl == 2 then
-	bl "I see an annoying girl running toward me from the distance, waving her arms in the air like she's totally oblivious to any attention she might draw to herself."
+		bl "Vedo una ragazza fastidiosa che corre verso di me da lontano, agitando le braccia in aria come se fosse completamente ignara dell'attenzione che potrebbe attirare su di sé."
 	elseif cl == 3 then
-	cw('bl',"That girl is "..s_name..", my neighbor and good friend since we were children.")
+		cw('bl',"Quella ragazza è "..s_name..", la mia vicina e amica fin da quando eravamo bambini.")
 	elseif cl == 4 then
-	bl "You know, the kind of friend you'd never see yourself making today, but it just kind of works out because you've known each other for so long?"
+		bl "Sai, il tipo di amico che non ti saresti mai aspettato di fare oggi, ma che funziona solo perché ci conosciamo da così tanto tempo?"
 	elseif cl == 5 then
-	bl "We used to walk to school together on days like this, but starting around high school she would oversleep more and more frequently, and I would get tired of waiting up."
+		bl "Un tempo camminavamo insieme a scuola in giorni come questo, ma a partire dalle scuole superiori lei iniziò a dormire sempre più tardi e io mi stancavo di aspettare."
 	elseif cl == 6 then
-	bl "But if she's going to chase after me like this, I almost feel better off running away."
+		bl "Ma se lei vuole inseguirmi in questo modo, mi sento quasi meglio a scappare via."
 	elseif cl == 7 then
-	cw('bl',"However, I just sigh and idle in front of the crosswalk and let "..s_name.." catch up to me.")
-	event_init('s_glitch')
+		cw('bl',"Tuttavia, sospiro e mi fermo davanti al semaforo e lascio che "..s_name.." mi raggiunga.")
+		event_init('s_glitch')
 	elseif cl == 8 then
-	if gs == 0 then
-		audioUpdate('2gs')
-	elseif gs == 1 then
-		audioUpdate('2gs2')
-	else
-		audioUpdate('2gs3')
-	end
-	event_start('s_glitch')
+		if gs == 0 then
+			audioUpdate('2gs')
+		elseif gs == 1 then
+			audioUpdate('2gs2')
+		else
+			audioUpdate('2gs3')
+		end
+		event_start('s_glitch')
 	end
 end
